a12c5ecd6018b9ca34adf3209999519db86137a3
James Edward Dillard <jamesedwarddillard@gmail.com>
2014-09-03 18:14:22 -0700

    Created a test to make sure relative discounts were applied to the full item price

---
+++ test_discount_calculator.py
@@ -0,0 +1,11 @@
+import unittest
+
+from discount_calculator import discount_calc
+
+class DiscountCalculatorTests(unittest.TestCase):
+	def testRelativeDiscountApplication(self):
+		# Ensures that relative discounts get applied to the total item cost not the item cost after other discounts
+		cost = discount_calc(100, 10, 10)
+		self.assertEqual(cost, 80)
+if __name__ == "__main__":
+	unittest.main()
